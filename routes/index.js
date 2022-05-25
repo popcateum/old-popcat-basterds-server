7d04ce25b7b57fd0e38511e8ee6931be0236154f
john <25576054+GaeJobBu@users.noreply.github.com>
2022-05-26 02:25:36 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 router.get("/", (req, res) => {
     console.log("Helth Check");
-    console.log(req);
+    //console.log(req);
     res.status(200).send("Hello Old PopCat Bastards!");
 });
 
