a8276bc232fc91edb693bdc958a4f8d1c2899bd7
john <25576054+GaeJobBu@users.noreply.github.com>
2022-05-26 02:25:23 +0900

    prod cors

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 const indexRouter = require('./routes/index')
-const whitelistRouter = require('./routes/whitelist')
+const whitelistRouter = require
+
+let corsOptions = {
+    origin: 'https://*.oldpopcatbasterds.com',
+    credentials: true
+}('./routes/whitelist')
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use('/', indexRouter);
 app.use('/whitelist', whitelistRouter);
 
 app.listen(3330, () => {
-    let host = "http://localhost:3330";
+    //let host = "http://localhost:3330";
     console.log("env : " + process.env.TITLE);
 })
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 router.get("/", (req, res) => {
-    console.log("Ping " + req.get('host') + " from " + req.get('origin'));
+    console.log("Helth Check");
     console.log(req);
     res.status(200).send("Hello Old PopCat Bastards!");
 });
