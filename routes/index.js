83e7ff746644326d386f7ae117949ec48f1af4f8
john <25576054+GaeJobBu@users.noreply.github.com>
2022-05-25 17:19:44 +0900

    origin check

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 router.get("/", (req, res) => {
+    console.log("hello " + req.get('host') + " from " + req.get('origin'));
     res.status(200).send("Hello Old PopCat Bastards!");
 });
 
