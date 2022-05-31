14ea31442e81fdd1658f9844c8264f9bb0ef98b4
john <25576054+GaeJobBu@users.noreply.github.com>
2022-06-01 00:00:09 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const indexRouter = require('./routes/index');
 const whitelistRouter = require('./routes/whitelist');
 
 let corsOptions = {
-    origin: 'https://*.oldpopcatbasterds.com',
+    origin: '*.oldpopcatbasterds.com',
     credentials: true
 }
 
