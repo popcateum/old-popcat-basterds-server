dadffa14385afe21fdc95a4dafc7bf5c54092ffd
jobbu <25576054+GaeJobBu@users.noreply.github.com>
2022-07-16 01:26:08 +0900

    dynamic cors test for xpass

--- app.js
+++ app.js
@@ -4,17 +4,38 @@ const cors = require("cors");
 const indexRouter = require('./routes/index');
 const whitelistRouter = require('./routes/whitelist');
 const metadataRouter = require('./routes/metadata');
+const xpassRouter = require('./routes/xpass');
 
-let corsOptions = {
-    origin: 'https://oldpopcatbasterds.wtf',
-    credentials: true
+// let corsOptions = {
+//     origin: 'https://oldpopcatbasterds.wtf',
+//     credentials: true
+// }
+
+const allowlist = ['https://oldpopcatbasterds.wtf', 'https://v2.dogesound.club'];
+
+const corsOptionsDelegate = (req, callback) => {
+    let corsOptions;
+
+    let isDomainAllowed = allowlist.indexOf(req.header('Origin')) !== -1;
+
+    if (isDomainAllowed) {
+        corsOptions = { 
+            origin: true ,
+            credentials: true
+        }
+    } else {
+        corsOptions = { origin: false }
+    }
+    callback(null, corsOptions)
 }
 
 const app = express();
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
+app.use(cors(corsOptionsDelegate));
 app.use('/', indexRouter);
 app.use('/whitelist', whitelistRouter);
 app.use('/metadata', metadataRouter);
+app.use('/xpass', xpassRouter);
 
 app.listen(3330, () => {
     //let host = "http://localhost:3330";
---
+++ routes/xpass.js
@@ -0,0 +1,22 @@
+let express = require('express');
+let router = express.Router();
+
+
+router.get("/:token_id", (req, res) => {
+    let token_id = req.params.token_id;
+    
+    if(token_id < 1 || token_id > 30000){
+        res.status(418).json({ message : "I'm a teapot"});
+        return;
+    }
+
+    //let metadata = new Metadata(token_id).toJSON();
+    //res.json(metadata);
+    res.json({"test":"test"});
+});
+
+router.get("/image", (req, res) => {
+    res.sendFile('../image/xpass.png')
+});
+
+module.exports = router;
