76eeeba298b4395c6bacd2980313154bea936b0b
jobbu <25576054+GaeJobBu@users.noreply.github.com>
2022-07-16 02:22:03 +0900

    remove cors lib

--- app.js
+++ app.js
@@ -1,38 +1,23 @@
 require('dotenv').config();
 const express = require("express");
-const cors = require("cors");
 const indexRouter = require('./routes/index');
 const whitelistRouter = require('./routes/whitelist');
 const metadataRouter = require('./routes/metadata');
 const xpassRouter = require('./routes/xpass');
 
-// let corsOptions = {
-//     origin: 'https://oldpopcatbasterds.wtf',
-//     credentials: true
-// }
-
-const allowlist = ['https://oldpopcatbasterds.wtf', 'https://v2.dogesound.club', 'https://egsa.io/'];
-
-const corsOptionsDelegate = (req, callback) => {
-    let corsOptions;
-
-    console.log(req)
-    let isDomainAllowed = allowlist.indexOf(req.header('origin')) !== -1;
-
-    if (isDomainAllowed) {
-        corsOptions = { 
-            origin: true ,
-            credentials: true
-        }
-    } else {
-        corsOptions = { origin: false }
-    }
-    callback(null, corsOptions)
-}
-
+const allowedOrigins = ['https://oldpopcatbasterds.wtf', 'https://v2.dogesound.club', 'https://egsa.io/'];
 const app = express();
-//app.use(cors(corsOptions));
-app.use(cors(corsOptionsDelegate));
+
+app.use(function (req, res, next) {
+    if(allowedOrigins.indexOf(req.headers.origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    }
+    next();
+})
+
 app.use('/', indexRouter);
 app.use('/whitelist', whitelistRouter);
 app.use('/metadata', metadataRouter);
